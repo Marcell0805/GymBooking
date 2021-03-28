19f01ff52ddc7d7e70a6b2967943800baffe35c1
M@rcell <marcellvanniekerk@gmail.com>
2021-03-28 18:53:11 +0200

    Initial

--- 1. Source/1. Code/3. DAL/GymBooking.DAL/GymBooking.Data/GymContext.cs
+++ 1. Source/1. Code/3. DAL/GymBooking.DAL/GymBooking.Data/GymContext.cs
@@ -9,5 +9,11 @@ namespace GymBooking.Data
         public DbSet<GymArea> Areas { get; set; }
         public DbSet<Member> Members { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source =(localdb)\\MSSQLLocalDB; Initial Catalog = GymBookingData");
+            
+        }
     }
 }
---
+++ 1. Source/1. Code/3. DAL/GymBooking.DAL/GymBooking.Data/Migrations/20210328164446_init.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymBooking.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Areas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AreaName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AllowedSessions = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Areas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<double>(type: "float", nullable: false),
+                    AreaIdId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_Areas_AreaIdId",
+                        column: x => x.AreaIdId,
+                        principalTable: "Areas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemberName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MemberSurname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BookingIdId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Members_Bookings_BookingIdId",
+                        column: x => x.BookingIdId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_AreaIdId",
+                table: "Bookings",
+                column: "AreaIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_BookingIdId",
+                table: "Members",
+                column: "BookingIdId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Members");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "Areas");
+        }
+    }
+}
